0eda68982fe8b8325156610208d53b1c035e2bdb
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-12-06 08:17:25 +0700

    feat(customer) add: router

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -3,10 +3,11 @@ package http
 import "github.com/gofiber/fiber/v2"
 
 type Router struct {
-	App               *fiber.App
-	BrandController   *BrandController
-	VoucherController *VoucherController
-	AuthMiddleware    fiber.Handler
+	App                *fiber.App
+	BrandController    *BrandController
+	VoucherController  *VoucherController
+	CustomerController *CustomerController
+	AuthMiddleware     fiber.Handler
 }
 
 type router interface {
@@ -15,12 +16,13 @@ type router interface {
 	registerPrivateEndpoints()
 }
 
-func NewRouter(app *fiber.App, brandController *BrandController, voucherController *VoucherController, authMiddleware fiber.Handler) router {
+func NewRouter(app *fiber.App, brandController *BrandController, voucherController *VoucherController, customerController *CustomerController, authMiddleware fiber.Handler) router {
 	return &Router{
-		App:               app,
-		BrandController:   brandController,
-		VoucherController: voucherController,
-		AuthMiddleware:    authMiddleware,
+		App:                app,
+		BrandController:    brandController,
+		VoucherController:  voucherController,
+		CustomerController: customerController,
+		AuthMiddleware:     authMiddleware,
 	}
 }
 
@@ -42,13 +44,19 @@ func (r *Router) registerPrivateEndpoints() {
 
 // registerPublicEndpoints implements router.
 func (r *Router) registerPublicEndpoints() {
+	//brand
 	r.App.Post("/brands", r.BrandController.CreateBrand)
 	r.App.Get("/brands/:id", r.BrandController.GetBrandByID)
 	r.App.Put("/brands/:id", r.BrandController.UpdateBrand)
 	r.App.Delete("/brands/:id", r.BrandController.DeleteBrand)
+	//voucher
 	r.App.Post("/vouchers", r.VoucherController.CreateVoucher)
 	r.App.Get("/vouchers/brand", r.VoucherController.GetAllByBrand)
 	r.App.Get("/vouchers/:id", r.VoucherController.GetVoucherByID)
 	r.App.Put("/vouchers/:id", r.VoucherController.UpdateVoucher)
 	r.App.Delete("/vouchers/:id", r.VoucherController.DeleteVoucher)
+	//customer
+	r.App.Post("/customers", r.CustomerController.CreateCustomer)
+	r.App.Get("/customers/:id", r.CustomerController.GetCustomerByID)
+
 }
