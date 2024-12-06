fece61283ed78765c8b880340102b23f0dafd3cf
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-12-06 13:48:41 +0700

    feat(transaction) add: repository

---
+++ internal/repository/transaction_repository.go
@@ -0,0 +1,60 @@
+package repository
+
+import (
+	"errors"
+
+	"github.com/KurniawanMuhammadRizki/simple-go-be/internal/entity"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type TransactionRepository struct {
+	Log *logrus.Logger
+}
+
+func NewTransactionRepository(log *logrus.Logger) *TransactionRepository {
+	return &TransactionRepository{
+		Log: log,
+	}
+}
+
+func (b *TransactionRepository) Save(db *gorm.DB, transaction *entity.Transaction) (*entity.Transaction, error) {
+	err := db.Create(transaction).Error
+	if err != nil {
+		b.Log.Error(err)
+		return nil, err
+	}
+
+	return transaction, nil
+}
+
+// GetByID implements TransactionRepository.
+func (b *TransactionRepository) GetByID(db *gorm.DB, id int64) (*entity.Transaction, error) {
+	var transaction entity.Transaction
+	err := db.Where("id = ?", id).First(&transaction).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			b.Log.WithField("id", id).Error("transaction not found")
+			return nil, err
+		}
+		b.Log.Error(err)
+		return nil, err
+	}
+	return &transaction, nil
+}
+
+func (b *TransactionRepository) GetAllByCustomer(db *gorm.DB, CustomerID int64) ([]entity.Transaction, error) {
+	var transactions []entity.Transaction
+
+	err := db.Where("customer_id = ?", CustomerID).Find(&transactions).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			b.Log.WithField("customer_id", CustomerID).Error("no transactions found for the given customer")
+			return nil, err
+		}
+		b.Log.Error(err)
+		return nil, err
+	}
+
+	return transactions, nil
+}
