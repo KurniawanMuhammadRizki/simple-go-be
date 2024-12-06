7cd129d77d813be70f741f69281e984a1bd43d2a
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-12-06 15:34:49 +0700

    feat(transactionDetail) add: model

---
+++ internal/model/transactionDetail.go
@@ -0,0 +1,18 @@
+package model
+
+type CreateTransactionDetailRequest struct {
+	TransactionID int64 `json:"transaction_id" validate:"required"`
+	VoucherID     int64 `json:"voucher_id" validate:"required"`
+	CostInPoint   int64 `json:"cost_in_point" validate:"required"`
+	Quantity      int64 `json:"quantity" validate:"required"`
+	SubTotalCost  int64 `json:"sub_total_cost" validate:"required"`
+}
+
+type CreateTransactionDetailResponse struct {
+	ID            int64  `json:"id"`
+	TransactionID int64  `json:"transaction_id"`
+	VoucherID     int64  `json:"voucher_id"`
+	Quantity      int64  `json:"quantity"`
+	SubTotalCost  int64  `json:"sub_total_cost"`
+	CreatedAt     string `json:"created_at"`
+}
